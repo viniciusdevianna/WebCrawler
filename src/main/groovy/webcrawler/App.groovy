fed34b4a872a5456c2ea8809ed4729983cb56eac
viniciusdevianna <vinidevianna@gmail.com>
2023-10-15 21:24:27 -0300

    feat: Tasks 2 e 3 finalizadas

--- src/main/groovy/webcrawler/App.groovy
+++ src/main/groovy/webcrawler/App.groovy
@@ -34,8 +34,8 @@ Document padraoTissPage = HttpBuilder.configure {
     request.uri = tissFiles[0].attr("href")
 }.get() as Document
 
-Elements tableRows = padraoTissPage.select("tbody > tr")
-String componenteComunicacaoLink = tableRows.find {
+Elements filesTableRows = padraoTissPage.select("tbody > tr")
+String componenteComunicacaoLink = filesTableRows.find {
         it.select("td").first().html() == "Componente de Comunicação"
     }.select("td > a").first().attr("href")
 
@@ -48,4 +48,56 @@ HttpBuilder.configure {
     file.createNewFile()
     Download.toFile(delegate as HttpConfig, file)
     println "${fileName} salvo na pasta Downloads"
+}
+
+Document historicoTissPage = HttpBuilder.configure {
+    request.uri = tissFiles[1].attr("href")
+}.get() as Document
+
+Elements compenteciasTableRows = historicoTissPage.select("tbody > tr")
+Integer lastDateRowIndex = compenteciasTableRows.findIndexOf {
+    it.select("td")
+            .first()
+            .html()
+            .toLowerCase()
+            .replace("<span>", "")
+            .replace("</span>", "") == "jan/2016"
+}
+
+File historico = new File("./Downloads/Arquivos_padrao_TISS/historico.csv")
+if (!historico.exists()) {
+    historico.createNewFile()
+    historico.with {
+        it.append("Competência;Publicação;Início de Vigência;\n")
+    }
+}
+
+historico.with {file ->
+    compenteciasTableRows.subList(0, lastDateRowIndex).each {
+        it.select("td").subList(0, 3).each { element ->
+            file.append(
+                    element
+                            .html()
+                            .toLowerCase()
+                            .replace("<span>", "")
+                            .replace("</span>", "")
+                            + ";"
+            )
+        }
+        file.append("\n")
+    }
+}
+
+Document tabelaErrosPage = HttpBuilder.configure {
+    request.uri = tissFiles[2].attr("href")
+}.get() as Document
+
+String tabelaErrosLink = tabelaErrosPage.select("p.callout > a").first().attr("href")
+HttpBuilder.configure {
+    request.uri = tabelaErrosLink
+}.get {
+    String filename = tabelaErrosLink.split("/").last()
+    File file = new File("./Downloads/Arquivos_padrao_TISS/${filename}")
+    file.createNewFile()
+    Download.toFile(delegate, file)
 }
